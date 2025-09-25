e71bed07c12927488d1c26c928117f95f99c2d5f
JBrij <justinbrij@yahoo.com>
2025-09-25 11:52:36 -0400

    Add root URL to fix Not Found

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('', RedirectView.as_view(url='/polls/')),  # Redirect root to /polls/
 ]
