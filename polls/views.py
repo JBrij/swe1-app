e28699b266f0f32edeaad2d21897ce14a4508226
JBrij <justinbrij@yahoo.com>
2025-10-22 19:29:18 -0400

    Format code with Black, fix Flake8 issues, add coverage configuration

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,9 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-
-from .models import Choice, Question
-
-
-from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
-
 from .models import Choice, Question
+from django.views import generic
 
 
 class IndexView(generic.ListView):
